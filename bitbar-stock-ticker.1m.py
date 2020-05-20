7a0cbdced58c7c349361a5bb339bab85b14c328a
Nozomi Okada <nokada@proofpoint.com>
2020-05-19 22:37:22 -0600

    Removing price change in percent

--- bitbar-stock-ticker.1m.py
+++ bitbar-stock-ticker.1m.py
@@ -9,17 +9,17 @@ RESET = '\033[0m'
 # Enter your stock symbols here in the format: ["symbol1", "symbol2", ...]
 stock_symbols = ['PFPT']
 
-api = 'https://query1.finance.yahoo.com/v7/finance/quote?'
+api_url = 'https://query1.finance.yahoo.com/v7/finance/quote?'
 fields = ['regularMarketPrice', 'regularMarketChange', 'regularMarketChangePercent']
 
-with urllib.request.urlopen(f'{api}symbols={",".join(stock_symbols)}&fields={",".join(fields)}') as response:
+with urllib.request.urlopen(f'{api_url}symbols={",".join(stock_symbols)}&fields={",".join(fields)}') as response:
     json_data = json.loads(response.read())
 
 for stock_quote in json_data["quoteResponse"]["result"]:
     stock_symbol = stock_quote["symbol"]
     price_current = stock_quote["regularMarketPrice"]
     price_change = stock_quote["regularMarketChange"]
-    price_percent_change = stock_quote["regularMarketChangePercent"]
+    price_change_percent = stock_quote["regularMarketChangePercent"]
 
     color = ''
     change = float(price_change)
@@ -31,7 +31,6 @@ for stock_quote in json_data["quoteResponse"]["result"]:
 
     price_current = '{:.2f}'.format(price_current)
     price_change = color + '{:.2f}'.format(price_change) + RESET
-    price_percent_change = '(' + color + '{:.2f}%'.format(price_percent_change) + RESET + ')'
+    # price_change_percent = '(' + color + '{:.2f}%'.format(price_change_percent) + RESET + ')'
 
-    print(f'{stock_symbol}  {price_current} {price_change} {price_percent_change}')
-
+    print(f'{stock_symbol}  {price_current} {price_change}')
